8a498248cf7b6784a3921bae46f26f7fbba73a77
tasnim63 <tamanna202163@gmail.com>
2023-01-03 12:47:15 +0000

    leap year added

---
+++ index.js
@@ -0,0 +1,25 @@
+// function getRandomNumber(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min;
+//   }
+//   console.log(getRandomNumber(1,6));
+
+
+// const student = ["tamanna","sonia","Riya","keya"];
+// console.log(student.sort());
+
+
+
+// small number to big number serial
+// const roll_number = [34,55,3,3,12,35,76,74]
+// console.log(roll_number.sort(function(a,b){
+
+//   return b-a ;
+// }));
+
+// how to solve  leap year
+function isLeapYear(year){
+if( (year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))){
+
+}
+}
+isLeapYear(2026)
